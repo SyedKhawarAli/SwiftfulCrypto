9a6940a2e270f2b3ca38f3c2e6714696d8e5b12c
Khawar Ali <khawarali5@gmail.com>
2023-12-18 23:14:55 +0200

    Initial Commit

---
+++ SwiftfulCrypto/SwiftfulCryptoApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftfulCryptoApp.swift
+//  SwiftfulCrypto
+//
+//  Created by shah on 18.12.2023.
+//
+
+import SwiftUI
+
+@main
+struct SwiftfulCryptoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
