e5cb64ea99f003c2555a5314e1ee010e78b7a18c
Khawar Ali <khawarali5@gmail.com>
2023-12-21 19:51:38 +0200

    HomeViewModel setup

--- SwiftfulCrypto/Core/Home/Views/HomeView.swift
+++ SwiftfulCrypto/Core/Home/Views/HomeView.swift
@@ -9,6 +9,7 @@ import SwiftUI
 
 struct HomeView: View {
     
+    @EnvironmentObject private var homeViewModel: HomeViewModel
     @State private var showPortfolio: Bool = false
     
     var body: some View {
@@ -20,7 +21,15 @@ struct HomeView: View {
         // content
             VStack {
                 homeHeader
-                
+                columnTitles
+                if !showPortfolio {
+                    allCoinsList
+                    .transition(.move(edge: .leading))
+                }
+                if showPortfolio {
+                    portfolioCoinsList
+                        .transition(.move(edge: .trailing))
+                }
                 Spacer(minLength: 0)
             }
         }
@@ -32,6 +41,7 @@ struct HomeView: View {
         HomeView()
             .navigationBarHidden(true)
     }
+    .environmentObject(DeveloperPreview.instance.homeVM)
 }
 
 extension HomeView {
@@ -57,6 +67,41 @@ extension HomeView {
                     }
                 }
         }
+        .padding(.horizontal)
+    }
+    
+    private var allCoinsList: some View {
+        List {
+            ForEach(homeViewModel.allCoins){ coin in
+                CoinRowView(coin: coin, showHoldingColum: false)
+                    .listRowInsets(.init(top: 10, leading: 0, bottom: 10, trailing: 10))
+            }
+        }
+        .listStyle(.plain)
+    }
+    
+    private var portfolioCoinsList: some View {
+        List {
+            ForEach(homeViewModel.portfolioCoins){ coin in
+                CoinRowView(coin: coin, showHoldingColum: true)
+                    .listRowInsets(.init(top: 10, leading: 0, bottom: 10, trailing: 10))
+            }
+        }
+        .listStyle(.plain)
+    }
+    
+    private var columnTitles: some View {
+        HStack {
+            Text("Coin")
+            Spacer()
+            if showPortfolio {
+                Text("Holdings")
+            }
+            Text("Price")
+                .frame(width: UIScreen.main.bounds.width / 3.5, alignment: .trailing)
+        }
+        .font(.caption)
+        .foregroundColor(.theme.secondaryText)
         .padding(.horizontal)
     }
 }
--- SwiftfulCrypto/SwiftfulCryptoApp.swift
+++ SwiftfulCrypto/SwiftfulCryptoApp.swift
@@ -9,12 +9,16 @@ import SwiftUI
 
 @main
 struct SwiftfulCryptoApp: App {
+    
+    @StateObject private var homeViewModel = HomeViewModel()
+    
     var body: some Scene {
         WindowGroup {
             NavigationView {
                 HomeView()
                     .navigationBarHidden(true)
             }
+            .environmentObject(homeViewModel)
         }
     }
 }
