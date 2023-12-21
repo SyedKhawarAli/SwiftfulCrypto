7866a82bfe108c4e5c38fded8f1a49f3459b62c4
Khawar Ali <khawarali5@gmail.com>
2023-12-21 20:22:58 +0200

    CoinDataService setup

---
+++ SwiftfulCrypto/Services/CoinDataService.swift
@@ -0,0 +1,45 @@
+//
+//  CoinDataService.swift
+//  SwiftfulCrypto
+//
+//  Created by shah on 21.12.2023.
+//
+
+import Foundation
+import Combine
+
+class CoinDataService {
+    @Published var allCoins: [CoinModel] = []
+    var coinSubscription: AnyCancellable?
+    
+    init(){
+        getCoins()
+    }
+    
+    private func getCoins(){
+        guard let url = URL(string: "https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=250&page=1&sparkline=true&price_change_percentage=24h") else { return }
+        
+        coinSubscription = URLSession.shared.dataTaskPublisher(for: url)
+            .subscribe(on: DispatchQueue.global(qos: .default))
+            .tryMap { (output) -> Data in
+                guard let response = output.response as? HTTPURLResponse,
+                      response.statusCode >= 200 && response.statusCode < 300 else {
+                    throw URLError(.badServerResponse)
+                }
+                return output.data
+            }
+            .receive(on: DispatchQueue.main)
+            .decode(type: [CoinModel].self, decoder: JSONDecoder())
+            .sink { (completion) in
+                switch completion {
+                case .finished:
+                    break
+                case .failure(let failure):
+                    print(failure.localizedDescription)
+                }
+            } receiveValue: { [weak self] (returnedCoined) in
+                self?.allCoins = returnedCoined
+                self?.coinSubscription?.cancel()
+            }
+    }
+}
