ecf29e7005afd44d76cb4a7e72d053a9b4f87405
Khawar Ali <khawarali5@gmail.com>
2023-12-23 01:37:24 +0200

    Local File Manager setup

--- SwiftfulCrypto/Services/CoinImageService.swift
+++ SwiftfulCrypto/Services/CoinImageService.swift
@@ -14,21 +14,36 @@ class CoinImageService {
     @Published var image: UIImage? = nil
     private var imageSubscription: AnyCancellable?
     private var coin: CoinModel
-    
+    private let fileManager = LocalFileManager.instance
+    private let folderName = "coin_images"
+    private let imageName: String
+
     init(coin: CoinModel){
         self.coin = coin
+        self.imageName = coin.id
         getCoinImage()
     }
     
     private func getCoinImage(){
+        if let savedImage = fileManager.getImage(imageName: imageName, folderName: folderName){
+            self.image = savedImage
+        } else {
+            downloadCoinImage()
+        }
+    }
+    
+    private func downloadCoinImage(){
         guard let url = URL(string: coin.image) else { return }
+        
         imageSubscription = NetworkingManager.download(url: url)
             .tryMap({ data -> UIImage? in
                 return UIImage(data: data)
             })
             .sink(receiveCompletion: NetworkingManager.handleCompletion, receiveValue: { [weak self] (returnedImage) in
-                self?.image = returnedImage
-                self?.imageSubscription?.cancel()
+                guard let self, let returnedImage else { return }
+                self.image = returnedImage
+                self.imageSubscription?.cancel()
+                self.fileManager.saveImage(image: returnedImage, imageName: self.imageName, folderName: self.folderName)
             })
     }
 }
